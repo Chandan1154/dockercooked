2ae609236f76a0d7178f18477a20e292979cdea7
Chandan C C <chandan18cse@gmail.com>
2026-05-05 10:44:06 +0530

    Added docker secreate infor

--- src/main/java/com/docker/dockercooked/controller/HelloController.java
+++ src/main/java/com/docker/dockercooked/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
 public class HelloController {
@@ -11,5 +13,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, Docker!";
+    }
+
+    @GetMapping("/list")
+    public List<String> getList() {
+        return List.of("Item 1", "Item 2", "Item 3");
     }
 }
